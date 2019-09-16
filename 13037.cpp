d36d78141875919969c19b0f164523eff4c347d5
Alberto Redondo <albertomost@gmail.com>
2019-09-16 18:53:38 +0200

    Primera versión
    # Conflicts:
    #	13037.cpp

---
+++ 13037.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+#include <queue>
+#include <string>
+#include <unordered_set>
+
+using namespace std;
+
+void resuelve(int index) {
+
+	vector<unordered_set<int>> chocosExclusivos;
+	vector<unordered_set<int>> chocosFaltantes;
+
+	for (int i = 0; i < 3; ++i) {
+		unordered_set<int> chocos;
+		chocosExclusivos.push_back(chocos);
+	}
+
+	for (int i = 0; i < 3; ++i) {
+		unordered_set<int> chocos;
+		chocosFaltantes.push_back(chocos);
+	}
+
+	int l, r, s;
+
+	cin >> l >> r >> s;
+
+	for (int i = 0; i < l; ++i) {
+
+		int choco;
+
+		cin >> choco;
+
+		chocosExclusivos[0].insert(choco);
+
+	}
+
+	for (int i = 0; i < r; ++i) {
+
+		int choco;
+
+		cin >> choco;
+
+		if (chocosExclusivos[0].count(choco) == 1) {
+			chocosExclusivos[0].erase(choco);
+			chocosFaltantes[2].insert(choco);
+		}
+		else
+			chocosExclusivos[1].insert(choco);
+
+	}
+
+	for (int i = 0; i < s; ++i) {
+
+		int choco;
+
+		cin >> choco;
+
+		if (chocosExclusivos[0].count(choco) == 1) {
+			chocosExclusivos[0].erase(choco);
+			chocosFaltantes[1].insert(choco);
+		}
+		else if (chocosExclusivos[1].count(choco) == 1) {
+			chocosExclusivos[1].erase(choco);
+			chocosFaltantes[0].insert(choco);
+		}
+		else if (chocosFaltantes[2].count(choco) == 1)
+			chocosFaltantes[2].erase(choco);
+		else
+			chocosExclusivos[2].insert(choco);
+
+	}
+
+	cout << "Case #" << index + 1 << ":\n";
+
+	for (int i = 0; i < 3; ++i)
+		cout << chocosExclusivos[i].size() << ' ' << chocosFaltantes[i].size() << '\n';
+
+}
+
+int main() {
+
+	int nCasos;
+
+	cin >> nCasos;
+
+	for (int i = 0; i < nCasos; ++i)
+		resuelve(i);
+
+	return 0;
+
+}
