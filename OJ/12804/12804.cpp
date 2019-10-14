1803ec1e5d700d3d525b5f670f0833e5184bab3f
Alberto Redondo <albertomost@gmail.com>
2019-10-14 18:14:56 +0200

    OJ 1280 TODO

--- OJ/12804/12804.cpp
+++ OJ/12804/12804.cpp
@@ -1,8 +1,57 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+using vi = vector<int>;
+using vvi = vector<vi>;
+
+// buscar ciclos
+
+void resuelve() {
+
+	int lines;
+	bool termina = false;
+	char ins;
+
+	cin >> lines;
+
+	vvi adjList(lines + 1);
+
+	for (int i = 0; i < lines; ++i) {
+
+		cin >> ins;
+
+		if (ins == 'A' || ins == 'C')
+			adjList[i].push_back(i + 1);
+
+		if (ins == 'J' || ins == 'C') {
+
+			int next;
+
+			cin >> next;
+
+			if (next > lines)
+				termina = true;
+
+			adjList[i].push_back(next);
+
+		}
+
+	}
+
+	if (ins == 'A')
+		termina = true;
+
+}
+
 int main() {
+
+	int nCasos;
+
+	cin >> nCasos;
+
+	while (nCasos--) resuelve();
 
 	return 0;
 
