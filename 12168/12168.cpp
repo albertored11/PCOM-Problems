58adee5fa9f3d0fcbc4fe5b2ce1b63cae4bf7b54
Alberto Redondo <albertomost@gmail.com>
2019-10-28 19:47:31 +0100

    OJ 12168 ni idea

---
+++ 12168/12168.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <vector>
+#include <iomanip>
+#include <queue>
+#include <algorithm>
+#include <cstring>
+#include <cmath>
+#include <string>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <deque>
+#include <unordered_set>
+
+using namespace std;
+
+using ii = pair<int, int>;
+using vii = vector<ii>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vvii = vector<vii>;
+
+
+void resuelve() {
+
+    int cats, dogs, voters;
+    vvii grafo(cats + dogs);
+
+    cin >> cats >> dogs >> voters;
+
+    for (int i = 0; i < voters; ++i) {
+
+        string firstStr, secondStr;
+
+        cin >> firstStr >> secondStr;
+
+        if (firstStr[0] == 'C') {
+            grafo
+        }        
+
+    }
+
+}
+
+int main() {
+
+    int nCasos;
+
+    cin >> nCasos;
+
+    while (nCasos--) (resuelve());
+
+    return 0;
+
+}
