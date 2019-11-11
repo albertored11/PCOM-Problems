ae5e0c052c2caeab0d3aa3912022a7a16e492c61
Alberto Redondo <albertomost@gmail.com>
2019-11-11 17:49:42 +0100

    OJ 12168 WA

--- OJ/12168/12168.cpp
+++ OJ/12168/12168.cpp
@@ -23,26 +23,73 @@ using vii = vector<ii>;
 using vi = vector<int>;
 using vvi = vector<vi>;
 using vvii = vector<vii>;
+using ss = pair<string, string>;
+using vss = vector<ss>;
 
+int cats, dogs, voters, dogVoters;
+vvi adjList;
+vss votes;
+vi match, vis;
+
+int aug(int l) {
+
+	if (vis[l])
+		return 0;
+
+	vis[l] = 1;
+
+	for (auto r : adjList[l])
+		if (match[r] == -1 || aug(match[r])) {
+			match[r] = l;
+			return 1;
+		}
+
+	return 0;
+
+}
 
 void resuelve() {
 
-    int cats, dogs, voters;
-    vvii grafo(cats + dogs);
-
     cin >> cats >> dogs >> voters;
+
+    dogVoters = 0;
+
+    adjList.assign(voters, vi());
+    votes.clear();
 
     for (int i = 0; i < voters; ++i) {
 
-        string firstStr, secondStr;
+    	ss vote;
 
-        cin >> firstStr >> secondStr;
+    	cin >> vote.first >> vote.second;
 
-        if (firstStr[0] == 'C') {
-            grafo
-        }        
+    	if (vote.first[0] == 'D')
+			++dogVoters;
+
+    	for (int j = 0; j < votes.size(); ++j) {
+
+    		if (vote.first == votes[j].second || vote.second == votes[j].first) {
+
+    			adjList[i].push_back(j);
+				adjList[j].push_back(i);
+
+    		}
+
+    	}
+
+    	votes.push_back(vote);
 
     }
+
+	int mcbm = 0;
+	match.assign(voters, -1);
+
+	for (int l = 0; l < dogVoters; l++) {
+		vis.assign(dogVoters, 0);
+		mcbm += aug(l);
+	}
+
+	cout << voters - mcbm << '\n';
 
 }
 
@@ -52,7 +99,7 @@ int main() {
 
     cin >> nCasos;
 
-    while (nCasos--) (resuelve());
+    while (nCasos--) resuelve();
 
     return 0;
 
