53c980a62a9c5c623825fd9050a2ca20232492b1
Alberto Redondo <albertomost@gmail.com>
2019-11-21 22:03:27 +0100

    Plantilla añadida

---
+++ template.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <vector>
+#include <iomanip>
+#include <queue> // Cola de mayor a menor por defecto en priority
+#include <algorithm>
+#include <cstring>
+#include <cmath>
+#include <string>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <deque>
+#include <unordered_set>
+
+using namespace std;
+
+using ii = pair<int, int>;
+using vii = vector<ii>;
+using vi = vector<int>;
+using vvi = vector<vi>; // para listas de adyacencia
+
+int main() {
+    
+    return 0;
+    
+}    
