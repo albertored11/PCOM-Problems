7c3a87ff535a1022736d0dc7bff9d86a5ad6d4c1
Alberto Redondo <albertomost@gmail.com>
2019-12-15 13:25:17 +0100

    OJ 1160 ni idea

---
+++ OJ/1160/1160.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <vector>
+#include <iomanip>
+#include <queue> // Cola de mayor a menor por defecto en priority
+#include <algorithm>
+#include <cstring>
+#include <cmath>
+#include <string>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <deque>
+#include <unordered_set>
+
+using namespace std;
+
+using ii = pair<int, int>;
+using vii = vector<ii>;
+using vi = vector<int>;
+using vvi = vector<vi>; // para listas de adyacencia
+
+bool resuelve() {
+
+    int a;
+
+    cin >> a;
+
+    if (!cin)
+        return false;
+
+    int numPairs = 0;
+    unordered_set<int> compounds;
+
+    while (a != -1) {
+
+        int b;
+
+        cin >> b;
+        
+        
+
+        cin >> a;
+
+    }
+
+    return true;
+
+}
+
+int main() {
+
+    while (resuelve());
+    
+    return 0;
+    
+}    
